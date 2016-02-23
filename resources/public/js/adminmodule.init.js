4f69e2e17a383492c5ab21c2fecc648e24aa147f
Ross Moroney <code.ph0y@googlemail.com>
2016-02-23 16:08:07 +0000

    Create BootstrapDialog for Block User

--- resources/public/js/adminmodule.init.js
+++ resources/public/js/adminmodule.init.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 	$('.block-user').on('click', function() {
 		BootstrapDialog.show({
-			title: 'Say-hello dialog',
-			message: 'Hi Apple!'
+			title: 'Confirmation',
+			message: 'Are you sure you want to block user?',
+			closable: false,
+            buttons: [{
+                label: 'Yes',
+                cssClass: 'btn-success',
+                action: function(dialogRef) {
+                    //dialogRef.setClosable(true);
+                }
+            }, {
+                label: 'No',
+                cssClass: 'btn-warning',
+                action: function(dialogRef) {
+					dialogRef.close();
+                }
+            }]
 		});
 	});
 });
